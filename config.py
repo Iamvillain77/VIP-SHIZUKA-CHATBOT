0b4883575a4c2923e6534ca35059184bd91b9a9e
VILLAIN <iamrkay420@gmail.com>
2024-11-20 23:47:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "7891808190:AAFjtHi87BIGaDAghx9MosYhXxFAASuXE7Q")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(getenv("OWNER_ID", "5909658683"))
 SUPPORT_GRP = "oldskoolgc"
 UPDATE_CHNL = "iamvillain77"
