12a8eda811638bab72d3fef728ad0ac618affe0b
VILLAIN <iamrkay420@gmail.com>
2024-11-20 23:52:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7891808190:AAFjtHi87BIGaDAghx9MosYhXxFAASuXE7Q")
+BOT_TOKEN = getenv("BOT_TOKEN", "7891808190:AAHDqZiR1iiXlY1ZN2FRsET2Evpyj5PubBE")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(getenv("OWNER_ID", "5909658683"))
 SUPPORT_GRP = "oldskoolgc"
